6d7098a50edbed6639e303e505cb61a38b3f8aae
God-rep <gabrielgodoysilva@hotmail.com>
2025-07-06 10:16:57 -0300

    Progresso na obtenção do json de turmas

--- discentes-com-matricula-ativa-na-disciplina/discentes-com-matricula-ativa-na-disciplina.js
+++ discentes-com-matricula-ativa-na-disciplina/discentes-com-matricula-ativa-na-disciplina.js
@@ -1,34 +1,31 @@
-fetch('../disciplinas-perioso/turmas.json') // Adjust the path as needed
+
+let turmas;  // Contem todas as turmas de todos os professores
+
+fetch('../disciplinas-periodo/turmas.json') // Pega JSON das turmas
   .then(response => response.json())
-  .then(turmas => {
-    console.log(turmas);
+  .then(data => {
+    turmas = data;  // Joga em uma variavel turmas
+    console.log(turmas);      // Envia no log para checar erros
   })
   .catch(error => console.error('Error loading JSON:', error));
-
-
-
-
-
 
 
 //Inicia página pela primeira vez
 function iniciaPag(){}
 
+// Lista de todas as turmas do professor
+var listaTurmas = []
+
+//Lista de todos os alunos na turma
+var listaAlunos = []
+
+//Posição do primeiro aluno da visualização atual da tabela na lista de alunos 
+var paginaAtual = 0
 
 
-//Array de todos os alunos na disciplina
-var alunosTabela = []
 
-//Posição do primeiro aluno da visualização atual da tabela no array de alunos 
-var alunoCimaTabela = 0
-  
 //"< 10 Anteriores"
 function antPag(){}
 
 //"Proximos 10 >"
 function proxPag(){}
-
-
-//Aciona ao clicar nos botões de cada disciplina
-function mudaDisciplina(){}
-
